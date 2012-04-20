b41c2111ba8e26139edf14fa64292500f68362f7
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2012-04-20 16:50:20 -0400

    adding stuff

--- src/com/object/disoriented/ThreePSActivity.java
+++ src/com/object/disoriented/ThreePSActivity.java
@@ -8,6 +8,6 @@ public class ThreePSActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.buyer_start);
     }
 }
