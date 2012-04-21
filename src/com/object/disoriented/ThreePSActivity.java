3d0a30f5009c4a3dcb2a82f0b8b50b5ab0f1a802
unknown <Jason@Jason-PC.(none)>
2012-04-20 23:56:08 -0400

    ddwpew

--- src/com/object/disoriented/ThreePSActivity.java
+++ src/com/object/disoriented/ThreePSActivity.java
@@ -55,6 +55,7 @@ public class ThreePSActivity extends Activity {
             if (resultCode == RESULT_OK) {
                 String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                
                 // Handle successful scan
             } else if (resultCode == RESULT_CANCELED) {
                 // Handle cancel
