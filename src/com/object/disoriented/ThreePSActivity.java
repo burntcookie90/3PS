6797c8caa38280f075789788744ca9e92f889399
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2012-04-20 20:48:14 -0400

    added DB connector class

--- src/com/object/disoriented/ThreePSActivity.java
+++ src/com/object/disoriented/ThreePSActivity.java
@@ -1,5 +1,8 @@
 package com.object.disoriented;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,58 +13,59 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class ThreePSActivity extends Activity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 	private Button btnBuy;
 	private Button btnReceipt;
 	private String TAG = "3PS Buyer Screen";
-    @Override
-    
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.buyer_start);
-        
-        btnBuy = (Button) findViewById(R.id.btnBuy);
-        btnBuy.setOnClickListener(new OnClickListener() {
-			
+	@Override
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.buyer_start);
+
+		btnBuy = (Button) findViewById(R.id.btnBuy);
+		btnBuy.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Log.v(TAG,"Buy button pressed");
 				Toast.makeText(ThreePSActivity.this, "Buy Button clicked!", Toast.LENGTH_SHORT).show();
-				
+
 				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-		        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-		        startActivityForResult(intent, 0);
-		        //tryrtutr
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, 0);
+				//tryrtutr
 			}
 		});
-        
-        btnReceipt = (Button) findViewById(R.id.btnReceipt);
-        btnReceipt.setOnClickListener(new OnClickListener() {
-			
+
+		btnReceipt = (Button) findViewById(R.id.btnReceipt);
+		btnReceipt.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 
 				Log.v(TAG,"Receipt button pressed");
 				Toast.makeText(ThreePSActivity.this, "Receipt Button clicked!", Toast.LENGTH_SHORT).show();		
 			}
 		});
-        
-    }
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                String contents = intent.getStringExtra("SCAN_RESULT");
-                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                
-                Log.v(TAG,contents);
-                // Handle successful scan
-            } else if (resultCode == RESULT_CANCELED) {
-                // Handle cancel
-            }
-        }
-    }
+
+		DB newConnection = new DB();
+	}
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+				Log.v(TAG,contents);
+				// Handle successful scan
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+			}
+		}
+	}
 
 }
