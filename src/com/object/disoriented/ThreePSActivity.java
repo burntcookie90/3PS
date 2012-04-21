5c8b37db306bb77a23f2860dd81a3f047a9b46fa
unknown <Jason@Jason-PC.(none)>
2012-04-20 22:25:16 -0400

    Added barcode scanner

--- src/com/object/disoriented/ThreePSActivity.java
+++ src/com/object/disoriented/ThreePSActivity.java
@@ -1,6 +1,7 @@
 package com.object.disoriented;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,7 +27,12 @@ public class ThreePSActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Log.v(TAG,"Buy button pressed");
-				Toast.makeText(ThreePSActivity.this, "Buy Button clicked!", Toast.LENGTH_SHORT).show();				
+				Toast.makeText(ThreePSActivity.this, "Buy Button clicked!", Toast.LENGTH_SHORT).show();
+				
+				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		        startActivityForResult(intent, 0);
+		        //tryrtutr
 			}
 		});
         
@@ -36,10 +42,24 @@ public class ThreePSActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				
+
 				Log.v(TAG,"Receipt button pressed");
 				Toast.makeText(ThreePSActivity.this, "Receipt Button clicked!", Toast.LENGTH_SHORT).show();		
 			}
 		});
         
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                // Handle successful scan
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
+
 }
